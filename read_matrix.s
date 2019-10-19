c43c625970df257b1d422715faca06f19779906b
Collin Neale <collinneale@berkeley.edu>
2019-10-18 19:27:28 -0700

    part1

--- read_matrix.s
+++ read_matrix.s
@@ -19,14 +19,97 @@
 read_matrix:
 
     # Prologue
-	
+addi sp, sp, -8     #save a1,a2
+sw a1, 0(sp)
+sw a2 4(sp)
 
+add a1, x0, a0   #setup call for fopen
+add a2, x0, x0
 
+addi sp, sp, -4
+sw ra,0(sp)
+jal fopen
+lw ra,0(sp)
+addi sp, sp, 4
 
+addi, t4, x0, -1
+beq a0, t4, eof_or_error    #make sure file opened
 
+addi sp, sp, -4   #save fopen output
+sw a0, 0(sp)
 
+addi, a0, x0, 8    #set up call to malloc 2 4 byte numbers
 
+addi sp, sp, -4
+sw ra,0(sp)
+jal malloc
+lw ra,0(sp)
+addi sp, sp, 4
 
+add a2, a0, x0  #set a2 to the pointer to malloced output array
+lw a1, 0(sp)    #load fopen output to a1
+addi a3, x0, 8  #a3 means read 8 bytes
+
+addi sp, sp, -4
+sw ra,0(sp)
+jal fread
+lw ra,0(sp)
+addi sp, sp, 4
+
+addi, t4, x0, -1
+beq a0, t4, eof_or_error
+
+lw t0 0(a2)
+lw t1 4(a2) #set t0/t1 to row column vals
+
+lw t2 4(sp)
+lw t3 8(sp) #get a1/a2 from stack
+
+sw t0, 0(t2)
+sw t1, 0(t3) #store row/col in the a1/a2 array
+
+mul t0, t0, t1 #multiply t0 and t1
+addi sp, sp, -4 #save rowxcolumn
+sw t0, 0(sp)
+addi t4, x0, 4
+mul t0, t0, t4
+add, a0, x0, t0 #save number of bytes into a0 for malloc
+
+addi sp, sp, -4
+sw ra,0(sp)
+jal malloc
+lw ra,0(sp)
+addi sp, sp, 4
+
+addi sp, sp, -4 #save pointer to malloc
+sw a0, 0(sp)
+
+add a2, a0, x0  #set a2 to the pointer to malloced output array
+lw a1, 8(sp)    #load fopen output to a1
+
+lw a3, 4(sp)    #set a3 to rowxcolumns bytes
+addi t4, x0, 4
+mul a3, a3, t4
+
+addi sp, sp, -4
+sw ra,0(sp)
+jal fread
+lw ra,0(sp)
+addi sp, sp, 4
+
+bne a0, a3 eof_or_error #check number of bytes read = rowxcolumn
+
+lw a0, 0(sp)
+
+addi sp, sp, 12
+
+lw a1, 0(sp)
+lw a2 4(sp)
+
+addi sp, sp, 8
+
+addi t4, x0, 4
+mul t0, t0, t4
 
     # Epilogue
 
--- test_files/test_read_matrix.s
+++ test_files/test_read_matrix.s
@@ -7,10 +7,16 @@ file_path: .asciiz "./test_input.bin"
 .text
 main:
     # Read matrix into memory
+    la a0, file_path
+    addi a1, x0, 3
+    addi a2, x0, 3
 
+    jal ra, read_matrix
 
     # Print out elements of matrix
-
+    addi a1, x0, 3
+    addi a2, x0, 3
+    jal ra, print_int_array
 
     # Terminate the program
     addi a0, x0, 10
